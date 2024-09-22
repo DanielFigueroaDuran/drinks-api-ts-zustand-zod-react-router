9464a3839ca5d1537ac4ac3d3742f56f6fd1095d
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-22 17:52:59 +0200

    Creating the Search Form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,15 @@
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom"
 
 const Header = () => {
+      // const location = useLocation();
+      const { pathname } = useLocation();
+      const isHome = useMemo(() => pathname === '/', [pathname])
+      //console.log(isHome);
+
+      // console.log(location.pathname);
+      //console.log(pathname);
+
       return (
             <header className="bg-slate-800">
                   <div className="mx-auto container px-5 py-16">
@@ -8,10 +18,71 @@ const Header = () => {
                                     <img className="w-32" src="/logo.svg" alt="logo" />
                               </div>
 
-                              <nav className="">
-
+                              <nav className="flex gap-4 ">
+                                    <NavLink
+                                          to="/"
+                                          className={({ isActive }) =>
+                                                isActive ? ' text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+                                          }
+                                    >
+                                          Inicio
+                                    </NavLink>
+                                    <NavLink
+                                          to="/favoritos"
+                                          className={({ isActive }) =>
+                                                isActive ? ' text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'
+                                          }
+                                    >
+                                          Favotitos
+                                    </NavLink>
                               </nav>
                         </div>
+
+                        {isHome && (
+                              <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                                    <div className="space-y-4">
+                                          <label
+                                                className="block text-white uppercase font-extrabold text-lg"
+                                                htmlFor="ingredient"
+                                          >
+                                                Nobre o Ingredientes
+                                          </label>
+
+                                          <input
+                                                className="p-3 w-full rounded-lg focus:outline-none"
+                                                type="text"
+                                                name="ingredient"
+                                                id="ingredient"
+                                                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Café"
+                                          />
+                                    </div>
+
+                                    <div className="space-y-4">
+                                          <label
+                                                className="block text-white uppercase font-extrabold text-lg"
+                                                htmlFor="categories"
+                                          >
+                                                Categorias
+                                          </label>
+
+                                          <select
+                                                className="p-3 w-full rounded-lg focus:outline-none"
+                                                name="categories"
+                                                id="categories"
+
+                                          >
+                                                <option value="">-- Seleccione --</option>
+
+                                          </select>
+                                    </div>
+                                    <input
+                                          className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white 
+                                    font-extrabold w-full p-2 rounded-lg uppercase"
+                                          type="submit"
+                                          value="Buscar Resetas"
+                                    />
+                              </form>
+                        )}
                   </div>
             </header>
       )
