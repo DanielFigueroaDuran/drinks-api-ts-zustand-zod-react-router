aa54f461fad624803adaf12956f66f95e14504c6
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-09-24 20:54:29 +0200

    Finalizing the Code to display the drinks

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
-import { CategoriesApiResponseShema, DrinksAPIResponse, SearchFilterSchema } from "../utils/recipes-shema";
+import { CategoriesApiResponseShema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterSchema } from "../utils/recipes-shema";
 
 export type Categories = z.infer<typeof CategoriesApiResponseShema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchema>;
 export type Drinks = z.infer<typeof DrinksAPIResponse>;
+export type Drink = z.infer<typeof DrinkAPIResponse>;
